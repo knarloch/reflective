3f2286a20abd333d3921aa8da13e41f6f4e5b828
Krzysztof Narloch <knarloch+GITHUB@gmail.com>
2021-09-16 19:52:45 +0200

    Reworked toString to toTreelikeMultilineString
    Renamed class and modified the output to look like tree.

--- include/toString.hpp
+++ include/toString.hpp
@@ -6,35 +6,45 @@
 
 namespace reflective {
 
-struct ToStringContext
+struct ToTreelikeMultilineStringTransformation
 {
   std::string state;
 
-  ToStringContext() = default;
+  ToTreelikeMultilineStringTransformation() = default;
 
   template<typename ValueT>
-  explicit ToStringContext(const ValueT& value)
+  explicit ToTreelikeMultilineStringTransformation(const ValueT& value)
   {
     std::stringstream ss;
     ss << value;
     state = move(ss).str();
   }
 
+  static std::string addIndentation(std::string s)
+  {
+    s.insert(0 , 1u, '\n');
+    for (auto pos = 0; (pos = s.find('\n', pos)) < std::string::npos;) {
+      s.insert(pos+1 , 2u, ' ');
+      pos+=1;
+    }
+    return s;
+  }
+
   template<typename MemberT>
-  void applyMember(MemberT&& member, ToStringContext context)
+  void applyMember(MemberT&& member, ToTreelikeMultilineStringTransformation context)
   {
     std::stringstream ss;
-    ss << "\"" << member.getMemberName() << "\": " << move(context.state) << ", ";
+    ss << member.getMemberName() << ':' << addIndentation(move(context.state)) << '\n';
     state.append(ss.str());
   }
 
   template<typename MemberT>
-  void applyMember(MemberT&& member, vector<ToStringContext> contexts)
+  void applyMember(MemberT&& member, vector<ToTreelikeMultilineStringTransformation> contexts)
   {
     std::stringstream ss;
-    ss << "\"" << member.getMemberName() << "\": ";
+    ss << member.getMemberName() << ':';
     for (auto& context : contexts) {
-      ss << move(context.state) << ", ";
+      ss << addIndentation(move(context.state)) << '\n';
     }
     state.append(ss.str());
   }
@@ -42,9 +52,9 @@ struct ToStringContext
 
 template<typename Struct>
 std::string
-toString(const Struct& s)
+toTreelikeMultilineString(const Struct& s)
 {
-  return "{ " + move(forEachMember<ToStringContext>(s).state) + " }";
+  return ToTreelikeMultilineStringTransformation::addIndentation(move(forEachMember<ToTreelikeMultilineStringTransformation>(s).state));
 }
 
 }
---
+++ test/toTreelikeMultilineStringTest.cpp
@@ -0,0 +1,74 @@
+#include "toString.hpp"
+#include "Record.hpp"
+#include "gtest/gtest.h"
+
+TEST(toTreelikeMultilineStringTest, Record)
+{
+  Record r;
+  auto x = reflective::toTreelikeMultilineString(r);
+  EXPECT_EQ(typeid(x), typeid(std::string));
+  EXPECT_NE(x.find(r.id.getMemberName()), std::string::npos);
+  EXPECT_NE(x.find(std::to_string(r.id)), std::string::npos);
+  EXPECT_NE(x.find(std::to_string(r.someNumber)), std::string::npos);
+  EXPECT_NE(x.find(r.someText.getValue()), std::string::npos);
+}
+
+TEST(toTreelikeMultilineStringTest, RecordInRecord)
+{
+  RecordInRecord r;
+  auto x = reflective::toTreelikeMultilineString(r);
+
+  EXPECT_EQ(typeid(x), typeid(std::string));
+
+  auto cursor = 1u + x.find(r.firstRecord.getMemberName());
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(std::to_string(r.firstRecord().id), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(std::to_string(r.firstRecord().someNumber), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(r.firstRecord().someText(), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+
+  cursor = 1u + x.find(r.secondRecord.getMemberName(), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(std::to_string(r.secondRecord().id), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(std::to_string(r.secondRecord().someNumber), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(r.secondRecord().someText(), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+}
+
+TEST(toTreelikeMultilineStringTest, RecordVector)
+{
+  RecordVector r;
+  auto x = reflective::toTreelikeMultilineString(r);
+
+  EXPECT_EQ(typeid(x), typeid(std::string));
+
+  auto cursor = 1u + x.find(r.singleRecord.getMemberName());
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(std::to_string(r.singleRecord().id), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(std::to_string(r.singleRecord().someNumber), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(r.singleRecord().someText(), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+
+  cursor = 1u + x.find(r.recordVector.getMemberName(), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(std::to_string(r.recordVector()[0].id), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(std::to_string(r.recordVector()[0].someNumber), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(r.recordVector()[0].someText(), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(std::to_string(r.recordVector()[1].id), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(std::to_string(r.recordVector()[1].someNumber), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+  cursor = 1u + x.find(r.recordVector()[1].someText(), cursor);
+  EXPECT_NE(cursor, std::string::npos);
+}
